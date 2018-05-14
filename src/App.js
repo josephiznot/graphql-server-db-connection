b5444a67da89d6033bfa239000a20a86ff97f75b
Joseph Anderson <josephiznot@gmail.com>
2018-05-14 13:07:18 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./graphqllogo.png";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import GetUsers from "./components/queries/GetUsers";
+
+const client = new ApolloClient({ uri: "http://localhost:4001/graphql" });
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">GraphQL</h1>
+          </header>
+          <p className="App-intro">Joe's GraphQL Server</p>
+          {/* <GetUsers /> */}
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- src/components/queries/GetUsers.js
+++ src/components/queries/GetUsers.js
@@ -9,3 +9,23 @@ export const GET_USERS = gql`
     }
   }
 `;
+export default class GetUsers {
+  render() {
+    return (
+      <Query query={GET_USERS}>
+        {({ loading, error, data }) => {
+          if (data)
+            return (
+              <div>
+                <ul>
+                  <li>{data}</li>
+                </ul>
+              </div>
+            );
+          else if (error) return <h1>Error</h1>;
+          else return <h1>Loading...</h1>;
+        }}
+      </Query>
+    );
+  }
+}
